8de93d495f4c4cd360df9aec423defdf3869ee51
Finem50 <Finem50@live.com>
2019-02-14 19:21:00 -0600

    Finished implementing with functions and adding a bunch of comments for documentation. String input validator still pending.

--- gpacalc.cpp
+++ gpacalc.cpp
@@ -2,10 +2,11 @@
 #include <locale>
 #include <string>
 
-
-
 using namespace std;
 
+string numConvert(int a);
+int nameConvert(string s);
+double ratioCalc(int x, int y);
 
 int main(){
 
@@ -13,13 +14,13 @@ int main(){
 	string colorName = "";
 	char strBuffer[128] = {};
 	char strEntry[32] = {};
-	int num, denom, colorNum, length, i, strCount = 0;
+	int num, denom, numIn, colorNum, i, strCount = 0;
 	double ratio;
 	float gpa;
 
 	cout << "\nWelcome to the input validator!\n" << endl;
 
-	//Repeat loop until user terminates
+	//Repeat loop until user terminates with 'n' or any char except 'y'
 	while(choice[0] == 'y'){
 
 		cout << "Enter numerator: ";
@@ -35,23 +36,28 @@ int main(){
 
 			cin.clear();
 			cin.ignore(1000, '\n');
+		}
+
+		//Calculate ratio between num / denom
+		ratio = ratioCalc(num, denom);
+		if(ratio == -1){
+
+			cout << "Invalid input." << endl;
+		}else{
+
+			cout << "Ratio: " << ratio << endl;
+		}
+
+		//GPA will be float value restricted to three decimal places (x.xxx)
+		cout << "\nEnter GPA: ";
+		//Check for valid input type
+		if(!(cin >> gpa)){
+
+			cin.clear();
+			cin.ignore(1000, '\n');
 			cout << "Invalid input." << endl;
 		}
 
-			//Ratio should be determined by numerator / denominator
-			//If numerator > denominator then the input is invalid
-			if(num < denom){
-
-				ratio = num / (double)denom;
-				cout << "Ratio: " << ratio << endl;
-			}else{
-
-				ratio = 0;
-				cout << "Invalid input." << endl;
-			}
-
-		cout << "\nEnter GPA: ";
-		cin >> gpa;
 		//GPA input that is greater than 4.0 is invalid
 		if(gpa > 4.0){
 
@@ -62,76 +68,37 @@ int main(){
 			cout << "GPA: " << gpa << endl;
 		}
 
-		/* Different colors will correspond with different numbers, as follows:
-		2 == Blue
-		3 == Crimson
-		7 == Gold
-		12 == Lime
-		20 == Tan
-		**/
+
 		cout << "\nEnter color number: ";
-		cin >> colorNum;
-		cout << "Name: ";
+		//Check for valid input type
+		if(!(cin >> numIn)){
 
-		switch(colorNum){
+			cin.clear();
+			cin.ignore(1000, '\n');
 
-			case 2:
-				cout << "Blue" << endl;
-				break;
-
-			case 3:
-				cout << "Crimson" << endl;
-				break;
-
-			case 7:
-				cout << "Gold" << endl;
-				break;
-
-			case 12:
-				cout << "Lime" << endl;
-				break;
-
-			case 20:
-				cout << "Tan" << endl;;
-				break;
-
-			default:
-				cout << "Invalid input." << endl;;
-				break;
 		}
+		//Convert from color number -> name
+		colorName = numConvert(numIn);
+		cout << "Name: " << colorName << endl;
 
-		cout << "\nEnter color name: ";
-		cin >> colorName;
-		cout << "Color: ";
- 		/*
-		Nested if/else to match input with color number. It's not ideal but
-		it works.
- 		**/
-		if(colorName == "Blue" || colorName == "blue"){
 
-			cout << "2" << endl;
-			colorName = "";
-		}else if(colorName == "Crimson" || colorName == "crimson"){
+		cout << "Enter color name: ";
+		//Check for valid input type
+		if(!(cin >> colorName)){
 
-			cout << "3" << endl;
-			colorName = "";
-		}else if(colorName == "Gold" || colorName == "gold"){
-
-			cout << "7" << endl;
-			colorName = "";
-		}else if(colorName == "Lime" || colorName =="lime"){
-
-			cout << "12" << endl;
-			colorName = "";
-		}else if(colorName == "Tan" || colorName == "tan"){
-
-			cout << "20" << endl;
-			colorName = "";
-		}else{
+			cin.clear();
+			cin.ignore(1000, '\n');
+		}
+		//Convert from color name -> number
+		colorNum = nameConvert(colorName);
+		if(colorNum == -1){
 
 			cout << "Invalid input." << endl;
-			colorName = "";
+		}else{
+
+			cout << "Color: " << colorNum << endl;
 		}
+
 
 		cout << "\nEnter string: ";
 		cin >> strEntry;
@@ -148,11 +115,107 @@ int main(){
 			cout << strEntry[i];
 		}
 
-
+		//Prompt user for additional loop
 		cout << "\n\nMore data to enter? (y/n): ";
 		cin >> choice;
-
 	}
 
 	return 0;
+}
+
+/* Different colors will correspond with different numbers, as follows:
+2 == Blue
+3 == Crimson
+7 == Gold
+12 == Lime
+20 == Tan
+**/
+string numConvert(int a){
+
+	int num = a;
+	string color;
+
+	switch(a){
+
+		case 2:
+			color = "Blue";
+			break;
+
+		case 3:
+			color = "Crimson";
+			break;
+
+		case 7:
+			color = "Gold";
+			break;
+
+		case 12:
+			color = "Lime";
+			break;
+
+		case 20:
+			color = "Tan";
+			break;
+
+		default:
+			cout << "Invalid input." << endl;;
+			break;
+	}
+
+	return color;
+}
+
+//Conversion between color name and number is same as previously-mentioned
+int nameConvert(string s){
+
+	string name = s;
+	int num;
+
+
+	//Nested if/else to match input with color number.
+	if(name == "Blue" || name == "blue"){
+
+		num = 2;
+		name = "";
+	}else if(name == "Crimson" || name == "crimson"){
+
+		num = 3;
+		name = "";
+	}else if(name == "Gold" || name == "gold"){
+
+		num = 7;
+		name = "";
+	}else if(name == "Lime" || name =="lime"){
+
+		num = 12;
+		name = "";
+	}else if(name == "Tan" || name == "tan"){
+
+		num = 20;
+		name = "";
+	}else{
+
+		return -1;
+		name = "";
+	}
+
+	return num;
+}
+
+//Ratio should be determined by numerator / denominator
+//If numerator > denominator then the input is invalid (determined by -1)
+double ratioCalc(int x, int y){
+
+	int num_c = x, denom_c = y;
+	double ratio_c;
+
+	if(num_c < denom_c){
+
+		ratio_c = num_c / (double)denom_c;
+	}else{
+
+		ratio_c = -1;
+	}
+
+	return ratio_c;
 }
